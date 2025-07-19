bf5374c7b865199846f9504859f761aa29113bcd
Ella Zhao <146047825+Islene888@users.noreply.github.com>
2025-07-18 20:33:51 -0700

    Time: 370 ms (95.63%), Space: 0B (100%) - LeetHub

--- 1258-article-views-i/1258-article-views-i.sql
+++ 1258-article-views-i/1258-article-views-i.sql
@@ -1,7 +1,12 @@
-# Write your MySQL query statement below
-select distinct a.author_id as id
-from Views a
-join Views b
-where a.author_id = a.viewer_id
-and a.author_id = b.author_id
+-- # Write your MySQL query statement below
+-- select distinct a.author_id as id
+-- from Views a
+-- join Views b
+-- where a.author_id = a.viewer_id
+-- and a.author_id = b.author_id
+-- order by id asc
+
+select distinct author_id as id 
+from Views
+where author_id = viewer_id
 order by id asc
